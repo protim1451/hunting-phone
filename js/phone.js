239ecf0f27fadcfe85df7b1ed63d299336401059
protim1451 <partha.doict@gmail.com>
2024-02-28 01:21:54 +0600

    6th video

--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,30 @@
-const loadPhone = async (searchText) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
+const loadPhone = async (searchText, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     //console.log(phones);
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     //console.log(phones);
     const phoneContainer = document.getElementById('phone-container');
+
+    // Clear container
+    phoneContainer.textContent = '';
+
+    const showAllContainer = document.getElementById('show-all-container');
+    if(phones.length > 10 && !isShowAll){
+        showAllContainer.classList.remove('hidden');
+    }
+    else{
+        showAllContainer.classList.add('hidden');
+    }
+    //console.log('is show all', isShowAll);
+    if(!isShowAll){
+        phones = phones.slice(0,10);
+    }
+   
 
     phones.forEach(phone => {
         //console.log(phone);
@@ -20,22 +36,39 @@ const displayPhones = phones => {
                 <div class="card-body">
                     <h2 class="card-title">${phone.phone_name}</h2>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
+                    <div class="card-actions justify-center">
                         <button class="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
         `;
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+    // hide loading spinner
+    toggleLoadingSpinner(false);
 }
 
 // Handle Search Button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
+    toggleLoadingSpinner(true);
     //console.log('Search handle');
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     //console.log(searchText);
+    loadPhone(searchText, isShowAll);
 }
 
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+}
+// Handle Show All
 
-loadPhone();
+const handleShowAll = () => {
+    handleSearch(true);
+}
+//loadPhone();
